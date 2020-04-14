752eb09ed598103e055ce4a2af258e5189a83522
alister <alister.messi@gmail.com>
2020-04-14 22:10:57 +0100

    Get firebase expenses on start

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test'){
   require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === 'develpment'){
+} else 
+// if (process.env.NODE_ENV === 'develpment')
+{
   require('dotenv').config({ path: '.env.development' });
 }
 
